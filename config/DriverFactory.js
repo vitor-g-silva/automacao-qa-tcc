e70849582d1b36f024df94702e3024e79c309d37
Vitor Gonçalves da Silva <vitor.gds@outlook.com>
2026-07-14 17:13:28 -0300

    Alteracao instalacao Chrmoe

--- config/DriverFactory.js
+++ config/DriverFactory.js
@@ -1,12 +1,17 @@
 const { Builder } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const env = require('./env');
+const options = new chrome.Options();
+
+if (process.env.CHROME_BINARY) {
+  options.setChromeBinaryPath(process.env.CHROME_BINARY);
+}
 
 class DriverFactory {
   static async createDriver() {
     const options = new chrome.Options();
 
-    if (env.HEADLESS === true) {
+    if (env.HEADLESS === true) { 
       options.addArguments('--headless=new');
     }
     options.addArguments('--window-size=1920,1080');
