c6b3033c33e1070c9f695cd2aa3b03891fbbc444
Vitor Gonçalves da Silva <vitor.gds@outlook.com>
2026-07-14 17:20:35 -0300

    Alteração DriverFactory

--- config/DriverFactory.js
+++ config/DriverFactory.js
@@ -1,27 +1,39 @@
 const { Builder } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const env = require('./env');
-const options = new chrome.Options();
-
-if (process.env.CHROME_BINARY) {
-  options.setChromeBinaryPath(process.env.CHROME_BINARY);
-}
 
 class DriverFactory {
   static async createDriver() {
     const options = new chrome.Options();
 
-    if (env.HEADLESS === true) { 
-      options.addArguments('--headless=new');
+    if (process.env.CHROME_BINARY) {
+      options.setChromeBinaryPath(process.env.CHROME_BINARY);
     }
-    options.addArguments('--window-size=1920,1080');
+
+    const rodarHeadless = process.env.HEADLESS
+      ? process.env.HEADLESS === 'true'
+      : env.HEADLESS === true;
+
+    if (rodarHeadless) {
+      options.addArguments(
+        '--headless=new',
+        '--no-sandbox',
+        '--disable-dev-shm-usage',
+        '--window-size=1920,1080'
+      );
+    } else {
+      options.addArguments('--window-size=1920,1080');
+    }
 
     const driver = await new Builder()
       .forBrowser('chrome')
       .setChromeOptions(options)
       .build();
 
-    await driver.manage().window().maximize();
+    if (!rodarHeadless) {
+      await driver.manage().window().maximize();
+    }
+
     return driver;
   }
 }
